87acf6b0c90161c30ee3206fcf624e83067b1af7
miltonzini <miltonzini@gmail.com>
2024-02-29 19:35:08 -0300

    refactor Endpoints.cs

--- backend/academia2024/academia2024/endpoints/Endpoints.cs
+++ backend/academia2024/academia2024/endpoints/Endpoints.cs
@@ -6,10 +6,12 @@ namespace academia20204.Endpoints
     {
         public void AddRoutes(IEndpointRouteBuilder routes)
         {
-            routes.MapGet("/testHolaMundo", () => "Hello, world!");
-            routes.MapPost("/testHolaMundo", () => "Hello, world!");
-            routes.MapGet("/sumarUno/{numero}", (int numero) => (numero + 1).ToString());
-            routes.MapGet("/testDescribirPersona/{nombre}/{edad}/{ciudad}", (string nombre, int edad, string ciudad) => $"{nombre} tiene {edad} años y vive en {ciudad}.");
+            var app = routes.MapGroup("/api/test");
+
+            app.MapGet("/HolaMundo", () => "Hello, world!").WithTags("Hola Mundo");
+            app.MapPost("/HolaMundo", () => "Hello, world!").WithTags("Hola Mundo");
+            app.MapGet("/sumarUno/{numero}", (int numero) => (numero + 1).ToString()).WithTags("Otros");
+            app.MapGet("/DescribirPersona/{nombre}/{edad}/{ciudad}", (string nombre, int edad, string ciudad) => $"{nombre} tiene {edad} años y vive en {ciudad}.").WithTags("Otros");
 
         }
     }
