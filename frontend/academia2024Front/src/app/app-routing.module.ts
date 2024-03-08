d1685370bf24ff760330d67149821424dd74e0e8
miltonzini <miltonzini@gmail.com>
2024-03-08 19:52:50 -0300

    add basic routing and navigation

--- frontend/academia2024Front/src/app/app-routing.module.ts
+++ frontend/academia2024Front/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { UsuarioComponent } from './usuario/usuario.component';
+import { AuthComponent } from './auth/auth.component';
+import { ProductoComponent } from './producto/producto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'auth', component: AuthComponent},
+  { path: 'usuario', component: UsuarioComponent},
+  { path: 'producto', component: ProductoComponent},
+  { path: '**', component: HomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
